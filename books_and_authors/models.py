f59afaebfe218773cf03de6c3c39b0cae0542aae
Carlos Toledo <carlombo@hotmail.com>
2024-06-26 21:00:57 -0400

    [ADD] Se agrega modelo mucho a muchos

--- books_and_authors/models.py
+++ books_and_authors/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import MinValueValidator
 
 # Create your models here.
 class Client(models.Model):
@@ -24,3 +25,23 @@ c.created
 c.email = 'hola@hola.com'
 c.update
 '''
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    # books
+
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    year = models.IntegerField(validators=[MinValueValidator(-5000)])
+    author = models.ManyToManyField(Author, related_name='books', through='BookAuthor') # Una tabla intermedia
+
+# Si quiero agregar información extra, además de las llaves foráneas a la tabla intermedia,
+# la creamos de la siguiente forma:
+
+class BookAuthor(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    # Campos de extras de la tabla intermedia
+    is_main = models.BooleanField(default=True)
+    profit = models.IntegerField()
